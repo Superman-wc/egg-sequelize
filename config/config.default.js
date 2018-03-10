d8e2f6d4f9358cf01fbd570848e116d97cc34405
superman <bainx@126.com>
2018-03-10 17:10:14 +0800

    test

--- config/config.default.js
+++ config/config.default.js
@@ -1,10 +1,12 @@
 'use strict';
 
-exports.sequelize = {
-  dialect: 'mysql',
-  database: '',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
+exports.multipleSequelize = {
+	'default': {
+		dialect: 'mysql',
+		database: '',
+		host: 'localhost',
+		port: 3306,
+		username: 'root',
+		password: '',		
+	}
 };
